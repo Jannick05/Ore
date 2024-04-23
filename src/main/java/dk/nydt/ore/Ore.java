653805877bfa70fd08a209d8710e97ee795867dd
Jannick05 <Jannick_05@icloud.com>
2024-04-23 16:44:21 +0200

    Initial commit

---
+++ src/main/java/dk/nydt/ore/Ore.java
@@ -0,0 +1,17 @@
+package dk.nydt.ore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
